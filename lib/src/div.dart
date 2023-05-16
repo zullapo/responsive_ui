f7294de2aa09dc91bbb221d569f9e3f0ccebda9a
zullapo <pcluz62@gmail.com>
2023-05-16 16:36:23 -0300

    Grow width percentage

--- lib/src/div.dart
+++ lib/src/div.dart
@@ -9,12 +9,14 @@ class Div extends StatelessWidget {
   final Widget child;
   final Division division;
   final double additionalWidth;
+  final double growWidthPercentage;
 
   const Div({
     Key? key,
     this.division = const Division(),
     required this.child,
     this.additionalWidth = 0,
+    this.growWidthPercentage = 1,
   }) : super(key: key);
 
   @override
@@ -60,13 +62,13 @@ class Div extends StatelessWidget {
             : childWidth;
 
         return SizedBox(
-          width: recalculatedChildWidth + childOffset + additionalWidth,
+          width: (recalculatedChildWidth + childOffset + additionalWidth) * growWidthPercentage,
           child: Row(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               SizedBox(
-                width: recalculatedChildWidth + additionalWidth,
+                width: (recalculatedChildWidth + additionalWidth) * growWidthPercentage,
                 child: child,
               ),
             ],
